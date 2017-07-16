584a6e9ea174bf78f8cf74a3a2575863df86ebbf
williamd4112 <williamd4112@hotmail.com>
2017-07-16 22:44:33 +0800

    change to openai baseline api ocde

---
+++ soccer_env.py
@@ -0,0 +1,104 @@
+import numpy as np
+import os
+import gym
+
+import cv2
+
+from gym import error, spaces
+from gym import utils
+from gym.utils import seeding
+
+import pygame_soccer.soccer.soccer_environment as soccer_environment
+import pygame_soccer.soccer.soccer_renderer as soccer_renderer
+
+import logging
+logger = logging.getLogger(__name__)
+
+class ProcessSoccerFrame84(gym.ObservationWrapper):
+    def __init__(self, env=None):
+        super(ProcessSoccerFrame84, self).__init__(env)
+        self.observation_space = spaces.Box(low=0, high=255, shape=(84, 84, 1))
+
+    def _observation(self, obs):
+        return ProcessSoccerFrame84.process(obs)
+
+    @staticmethod
+    def process(frame):
+        img = frame
+        img = img[:, :, 0] * 0.299 + img[:, :, 1] * 0.587 + img[:, :, 2] * 0.114
+        resized_screen = cv2.resize(img, (84, 84), interpolation=cv2.INTER_AREA)
+        x_t = resized_screen
+        x_t = np.reshape(x_t, [84, 84, 1])
+        return x_t.astype(np.uint8)
+
+class SoccerEnv(gym.Env):
+    metadata = {'render.modes': ['human', 'rgb_array']}
+    
+    image_shape = (192, 288, 3)
+
+    action_set = [
+      'STAND',
+      'MOVE_RIGHT',
+      'MOVE_UP',
+      'MOVE_LEFT',
+      'MOVE_DOWN'
+    ]
+
+    action_meaning = [
+        'NOOP',
+        'MOVE_RIGHT',
+        'MOVE_UP',
+        'MOVE_LEFT',
+        'MOVE_DOWN'
+    ]
+
+    def __init__(self, frameskip=(2, 5), mode=None, viz=False):
+        assert mode in ['OFFENSIVE', 'DEFENSIVE', None]
+        self.frameskip = frameskip
+        self.mode = mode
+        self.viz = viz
+        
+        renderer_options = None
+        if viz:
+            renderer_options = soccer_renderer.RendererOptions(
+                show_display=True, max_fps=10, enable_key_events=True)
+
+        self.soccer_env = soccer_environment.SoccerEnvironment(renderer_options)
+    
+        self.action_space = spaces.Discrete(len(self.action_set))
+        self.observation_space = spaces.Box(low=0, high=255, shape=self.image_shape)
+
+    def _get_obs(self):
+        self.soccer_env.render()
+        return self.soccer_env.renderer.get_screenshot()
+
+    @property
+    def _n_actions(self):
+        return len(self.action_set)
+
+    def _reset(self):
+        self.soccer_env.reset()
+        return self._get_obs()
+
+    def _render(self, mode='human', close=False):
+        if self.viz:
+            self.soccer_env.render()
+
+    def _step(self, a):
+        reward = 0.0
+        action = self.action_set[a]
+
+        if isinstance(self.frameskip, int):
+            num_steps = self.frameskip
+        else:
+            num_steps = self.np_random.randint(self.frameskip[0], self.frameskip[1])
+        
+        for _ in range(num_steps):
+            ret = self.soccer_env.take_action(action)
+            reward += ret.reward
+        ob = self._get_obs()
+
+        return ob, reward, self.soccer_env.state.is_terminal(), None      
+    
+    def get_action_meanings(self):
+        return self.action_meaning 
---
+++ train_soccer.py
@@ -0,0 +1,45 @@
+import gym
+
+from baselines import deepq
+from baselines.common.atari_wrappers_deprecated import EpisodicLifeEnv, NoopResetEnv, MaxAndSkipEnv, FrameStack, ClippedRewardsWrapper, ScaledFloatFrame
+
+from soccer_env import SoccerEnv, ProcessSoccerFrame84
+
+def wrap_dqn_for_soccer(env):
+    """Apply a common set of wrappers for Atari games."""
+    env = NoopResetEnv(env, noop_max=30)
+    env = MaxAndSkipEnv(env, skip=4)
+    env = ProcessSoccerFrame84(env)
+    env = FrameStack(env, 4)
+    env = ClippedRewardsWrapper(env)
+    return env
+
+
+def main():
+    env = SoccerEnv(frameskip=1)
+    env = ScaledFloatFrame(wrap_dqn_for_soccer(env))
+    model = deepq.models.cnn_to_mlp(
+        convs=[(32, 8, 4), (64, 4, 2), (64, 3, 1)],
+        hiddens=[256],
+        dueling=True
+    )
+    act = deepq.learn(
+        env,
+        q_func=model,
+        lr=1e-4,
+        max_timesteps=2000000,
+        buffer_size=10000,
+        exploration_fraction=0.1,
+        exploration_final_eps=0.01,
+        train_freq=4,
+        learning_starts=10000,
+        target_network_update_freq=1000,
+        gamma=0.99,
+        prioritized_replay=True
+    )
+    act.save("soccer_model.pkl")
+    env.close()
+
+
+if __name__ == '__main__':
+    main()
